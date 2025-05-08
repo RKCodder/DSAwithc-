9343d2cff1f45d34b86fa54ebb4ded3f3e803291
RKCodder <126437618+RKCodder@users.noreply.github.com>
2025-05-08 02:34:25 +0000

    unaryopertor

---
+++ unary operator/post-increment.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b= a++; // 1stly assign the value then increments
+    cout<<b<<endl;// here 1stly assign a value in b then increment of a 
+    cout<<a<<endl;
+    return 0;
+
+}
---
+++ unary operator/pree-increment.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b= ++a;// update-> kaam
+cout<<b<<endl;
+cout<<a<<endl;
+return 0;
+}
